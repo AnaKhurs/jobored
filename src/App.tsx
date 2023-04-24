c2bdf0393c73dfbfb9aead9627c4d190dd1ca22d
AnaKhurs <ana.khurs@gmail.com>
2023-04-24 19:18:01 +0300

    Add navigation with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import {Header} from "./ui/features/Header/Header";
 import {SearchPage} from "./ui/pages/SearchPage";
+
+import {PATH} from "./utils/paths";
 
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <SearchPage/>
+        <div className="wrapper">
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path={PATH.MAIN} element={<SearchPage/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
