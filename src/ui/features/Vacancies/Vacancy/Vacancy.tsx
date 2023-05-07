6a5e9827289d895acda537534b972bbb20fa29a6
AnaKhurs <ana.khurs@gmail.com>
2023-05-07 10:44:33 +0300

    created FavoritesPage.tsx

--- src/ui/features/Vacancies/Vacancy/Vacancy.tsx
+++ src/ui/features/Vacancies/Vacancy/Vacancy.tsx
@@ -1,8 +1,8 @@
 import React, {memo} from "react";
+import {FavoriteButton} from "../../FavoriteButton/FavoriteButton";
 import {Flex, Group, List, Text} from "@mantine/core";
 import Svg from "../../../../img/Svg";
 import {useStyles} from "./styles";
-import {FavoriteButton} from "../../FavoriteButton/FavoriteButton";
 
 type PropsType = {
     profession: string
--- src/ui/pages/FavoritesPage/FavoritesPage.tsx
+++ src/ui/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,10 +1,22 @@
 import React, {memo} from "react";
+import {useNavigate} from "react-router-dom";
+import {Button, Flex, Text} from "@mantine/core";
+import Svg from "../../../img/Svg";
+import {PATH} from "../../../utils/paths";
 
 export const FavoritesPage = memo(() => {
 
+    const navigate = useNavigate();
+
+    const onClickToSearchPage = () => {
+        navigate(PATH.MAIN)
+    };
+
     return (
-        <div>
-            ку
-        </div>
+        <Flex direction="column" align="center" justify="center" h="75vh">
+            <Svg iconName="void"/>
+            <Text m="32px 0" fw={700} fz="xl" color="#343A40">Упс, здесь еще ничего нет!</Text>
+            <Button variant="light" onClick={onClickToSearchPage}>Поиск Вакансий</Button>
+        </Flex>
     );
 });
