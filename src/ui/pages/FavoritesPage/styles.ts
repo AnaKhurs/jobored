5952f9b1a6e0cc246dd337da2d4146f01d916517
AnaKhurs <ana.khurs@gmail.com>
2023-05-24 19:49:41 +0300

    FavoritePage.tsx: fix style

--- src/ui/pages/FavoritesPage/styles.ts
+++ src/ui/pages/FavoritesPage/styles.ts
@@ -4,9 +4,13 @@ export const useStyles = createStyles((theme) => ({
 
     resultsWrapper: {
         alignItems: "center",
-        margin: "0 28px",
+        width: "773px",
+        margin: "0 auto",
         [theme.fn.smallerThan(780)]: {
             margin: "0 18px",
+        },
+        [theme.fn.smallerThan(773)]: {
+            width: "100%",
         },
         [theme.fn.smallerThan(600)]: {
             margin: 0,
